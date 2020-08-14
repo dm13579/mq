dd1a82be647822e267d00bd6ae7bb6f04255e505
dm13579 <dongming053021@163.com>
2020-08-14 17:59:38 +0800

    feat(reliabledelivery): 消息的可靠性投递定时任务版本（模拟订单服务，商品服务分布式事务解决方案）项目搭建

---
+++ rabbitmq-reliabledelivery-timedtask-order/src/main/java/com/dm/mq/ReliabledeliveryTimedtaskOrderApplication.java
@@ -0,0 +1,18 @@
+package com.dm.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+  * @className ReliabledeliveryTimedtaskOrderApplication 
+  * @description q启动类
+  * @author dm
+  * @date 2020/8/14
+  * @since JDK1.8
+  */
+@SpringBootApplication
+public class ReliabledeliveryTimedtaskOrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReliabledeliveryTimedtaskOrderApplication.class,args);
+    }
+}
---
+++ rabbitmq-reliabledelivery-timedtask-product/src/main/java/com/dm/mq/ReliabledeliveryTimedtaskProductApplication.java
@@ -0,0 +1,18 @@
+package com.dm.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+  * @className ReliabledeliveryTimedtaskOrderApplication 
+  * @description q启动类
+  * @author dm
+  * @date 2020/8/14
+  * @since JDK1.8
+  */
+@SpringBootApplication
+public class ReliabledeliveryTimedtaskProductApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReliabledeliveryTimedtaskProductApplication.class,args);
+    }
+}
